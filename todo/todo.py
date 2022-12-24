dfe20a3243321d5c5d68312aef3bc57f9990c191
Zinaro <islamzinar@gmail.com>
2022-12-24 18:26:45 +0300

    todo

---
+++ todo/todo.py
@@ -0,0 +1,25 @@
+# !
+class ToDo:
+    # TODO nûçe
+    def nuce():
+        """
+        !ARMANC: komkirina nûçeyan
+        *1) listeya malperên nûçeyê bê amadekirin.
+        *2) ji bo her malperekî fonksiyonên cuda bê xebitandin
+        *3) malperên bi zimanê din bi google translate ê bê wergerkirin
+        *4) Piştê komkirina nûçeya hemû nûçe bişîne Firebase ê
+        """
+        pass
+    # TODO hevokên kurdî
+    def hevokenkurdi():
+        """
+        !ARMANC: komkirina 1.000.000 hevokên kurdî ye
+        *1) pdf ên pirtûkên kurdî ên ku tên xwendin were dîtin
+        *2) pdf ên kurdî were xwendin û ji hevokan de were qutkirin û mîna lîsteyekî bê qeydkirin
+        *3) ji beşa nûçeyan jî hevok bên komkirin ên ku ne bi kurdî bin bên wergerkirin
+        *4) hevokên ku dubare dikin wekî 1 hevok bê qeydkirin
+        *5) hevokên kurdî li cihekî bê qeydkirin
+        *6) 1 milyon hevok bê komkirin armanca vê beşê diqede
+        """
+        pass
+
